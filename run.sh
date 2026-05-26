38b00025f54f4e1cadd0542505c6da9432d73183
SleepyStar <reallegend6759@gmail.com>
2026-05-26 12:53:45 +0000

    changes done

--- run.sh
+++ run.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
 echo "Compiling and running standalone module simulation..."
 # iverilog -g2012 -I src -o test/sim.vvp src/project.v test/tb.v   # PATH CHANGES HERE
-iverilog -g2012 -I src -o test/sim.vvp src/basics-modules/basic-module1.v src/basics-testbenches/basic-testbench1.v
+iverilog -g2012 -I src -o src/basic-test/sim.vvp src/basics-modules/basic-module1.v src/basics-testbenches/basic-testbench1.v
 
 # note that for different files, the test bench and module arguments will change.
 
-vvp test/sim.vvp
+vvp src/basic-test/sim.vvp
 echo "Done! Click on the .vcd file to view waveforms."
 
 
+# USE THE ABOVE COMMANDS TO compile and run the simulation
 
-# NOTE- > to run this , run these commands on the github codespace terminal by creating a codespace terminal on this branch.
-# cd test/ 
-# bash run.sh 
+
