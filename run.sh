b5a91b41bfd6847b7075269b1ed79d208784cc0f
SleepyStar <reallegend6759@gmail.com>
2026-05-26 13:24:41 +0000

    Added 3 new practice files to basics track

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo "Compiling and running standalone module simulation..."
 # iverilog -g2012 -I src -o test/sim.vvp src/project.v test/tb.v   # PATH CHANGES HERE
+
+# make sure to be in the root directory of the project. Else paths will be different.
 iverilog -g2012 -I src -o src/basic-test/sim.vvp src/basics-modules/basic-module1.v src/basics-testbenches/basic-testbench1.v
 
 # note that for different files, the test bench and module arguments will change.
