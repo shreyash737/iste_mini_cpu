2a45ccb6908fb3191d6ba33110d72aeb3e4a48b0
SleepyCattStar <reallegend6759@gmail.com>
2026-05-26 17:59:30 +0530

    added half adder

--- src/basics-modules/basic-module1.v
+++ src/basics-modules/basic-module1.v
@@ -1,5 +1,13 @@
-module firstmodule()
+module firstmodule(
+    input a,
+    input b,
+    output sum,
+    output carry
+    
+)
+    assign sum = a+b;
+    assign carry = a & b;
 
-
+    // half adder
 
 endmodule
